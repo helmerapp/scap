588e636cd600cc2e6e14198843a732a7ce9afccd
ZohebMOPO <understandingzak@gmail.com>
2024-01-15 00:17:12 +0530

    feat: YUV conversion

--- src/capturer/engine/win/mod.rs
+++ src/capturer/engine/win/mod.rs
@@ -1,7 +1,10 @@
 use std::sync::mpsc;
-use std::error::Error;
+use std::{borrow::Borrow, error::Error};
 
-use crate::{capturer::Options, frame::Frame};
+use crate::{
+    capturer::Options,
+    frame::{Frame, YUVFrame},
+};
 use windows::Win32::Graphics::Gdi::{GetMonitorInfoW, HMONITOR, MONITORINFOEXW};
 use windows_capture::{
     capture::{CaptureControl, WindowsCaptureHandler},
@@ -30,9 +33,7 @@ impl WindowsCaptureHandler for Capturer {
     type Flags = mpsc::Sender<Frame>;
     type Error = Box<dyn std::error::Error + Send + Sync>;
 
-
     fn new(tx: Self::Flags) -> Result<Self, Self::Error> {
-
         Ok(Self { tx })
     }
 
@@ -41,11 +42,22 @@ impl WindowsCaptureHandler for Capturer {
         mut frame: &mut Wframe,
         _: InternalCaptureControl,
     ) -> Result<(), Self::Error> {
+        let mut frame_buffer = frame.buffer().unwrap();
+        let width = frame_buffer.width();
+        let height = frame_buffer.height();
 
-        let mut frame_buffer= frame.buffer().unwrap();
         let raw_frame_buffer = frame_buffer.as_raw_buffer();
         let frame_data = raw_frame_buffer.to_vec();
-        self.tx.send(Frame::BGR0(frame_data)).expect("Failed to send data");
+
+        let yuv_data = to_yuv(&raw_frame_buffer, width, height);
+
+        self.tx
+            .send(Frame::YUVFrame(yuv_data))
+            .expect("Failed to send data");
+
+        self.tx
+            .send(Frame::BGR0(frame_data))
+            .expect("Failed to send data");
         Ok(())
     }
 
@@ -54,7 +66,6 @@ impl WindowsCaptureHandler for Capturer {
         Ok(())
     }
 }
-
 
 impl WinStream {
     pub fn start_capture(&self) {
@@ -74,4 +85,38 @@ pub fn create_capturer(options: &Options, tx: mpsc::Sender<Frame>) -> WinStream 
     .unwrap();
 
     return WinStream { settings };
+}
+
+pub fn to_yuv(img: &[u8], width: u32, height: u32) -> YUVFrame {
+    let mut luminance_bytes: Vec<u8> = Vec::new();
+    let mut chrominance_bytes: Vec<u8> = Vec::new();
+
+    let mut index: usize = 0;
+    for y in 0..height {
+        for x in 0..width {
+            let r = img[index] as f64;
+            let g = img[index + 1] as f64;
+            let b = img[index + 2] as f64;
+            index += 1;
+
+            let y = 0.299 * r + 0.587 * g + 0.114 * b;
+            luminance_bytes.push(y as u8);
+
+            let u = -0.14713 * r + -0.28886 * g + 0.436 * b;
+            let v = 0.615 * r + -0.51499 * g + -0.10001 * b;
+
+            chrominance_bytes.push(u as u8);
+            chrominance_bytes.push(v as u8);
+        }
+    }
+
+    YUVFrame {
+        display_time: 0,
+        width: width as i32,
+        height: height as i32,
+        luminance_bytes,
+        luminance_stride: width as i32,
+        chrominance_bytes,
+        chrominance_stride: width as i32,
+    }
 }
